6715a371116490b589c03d990048d900f91995dd
Marveleecious <richardtimilehin05@gmail.com>
2022-10-20 04:52:15 -0700

    my first statement

---
+++ script.js
@@ -0,0 +1,29 @@
+
+function play() {
+    let input = document.getElementById('tinput');
+    let newInput = Array.from(input.value);
+    const random =[]
+    const predict = newInput;
+    for(let i =0; i  < 6; i++ ){
+        random[i] = Math.floor(Math.random() * 100);
+     }
+    
+    const correctGuess =[];
+    let correctIndex =0
+    for(let value of predict){
+        let numm = parseInt(value)
+        if(random.includes(numm)){
+            correctGuess[correctIndex] = value;
+            correctIndex++
+        }
+    }
+
+    if(correctGuess.length >=3){
+        document.getElementById('result').innerHTML = "YOU WIN"
+    }else{
+        document.getElementById('result').innerHTML = "OTI LULE"
+    }
+
+    document.getElementById('random').innerHTML = random
+    document.getElementById('avail-no').innerHTML = "<b>YOUR INPUT: " + correctGuess + "<b>" 
+}
